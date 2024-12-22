da56a152d67261e8631ed82cf6b5c6eddfc658bc
muhammed dursun <muhammedrsn35@gmail.com>
2024-12-22 21:19:07 +0300

    Project started.

---
+++ ChatApp-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp-iOS
+//
+//  Created by muhammed dursun on 22.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
